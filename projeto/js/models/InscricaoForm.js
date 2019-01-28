7f0487747350e74322ae5274d7ef69b76cfa4702
João Paulo Malvesti <jpmf1995@gmail.com>
2019-01-28 17:09:41 -0200

    atividade 4 - Criação de classe

---
+++ projeto/js/models/InscricaoForm.js
@@ -0,0 +1,25 @@
+class InscricaoForm {
+  constructor(nome, email, disponibilidade, interesses, mensagem) {
+    this.nome;
+    this.email;
+    this.disponibilidade;
+    this.interesses;
+    this.mensagem;
+  }
+  get nome(){
+    return this.nome;
+  }
+  get email(){
+    return this.email;
+  }
+  get disponibilidade(){
+    return this.disponibilidade;
+  }
+  get interesses(){
+    return this.interesses;
+  }
+  get mensagem(){
+    return this.mensagem;
+  }
+
+}
