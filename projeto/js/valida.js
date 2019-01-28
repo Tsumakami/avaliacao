3c61a35d2b05ad4ea5c1b3328c35bbfc9316efb9
João Paulo Malvesti <jpmf1995@gmail.com>
2019-01-28 17:11:36 -0200

    atividade 4 - validando Nome e email

--- projeto/js/valida.js
+++ projeto/js/valida.js
@@ -1,11 +1,21 @@
-var nome = $("#nome");
+
+let nome = $("#nome");
+let email = $("#email");
+/*
+let disponibilidade = $(".disponibilidade");
+let interesses = $(".interesses");
+
+
+
+*/
 
 function validaNome(){
   nome.on("change", function(){
-  var valor = nome.val();
+  let valor = nome.val();
+  var verifyInt = /\d+/g;
   var teste = valor.split(/\S+/).length - 1;
 
-  if(teste >= 2){
+  if(teste >= 2 && $(nome).val().match(verifyInt) == null){
     nome.removeClass("borda-vermelha");
     nome.addClass("borda-verde");
 
@@ -19,17 +29,50 @@ function validaNome(){
 
 });
 }
+validaNome();
 
 
+function validaEmail(){
+  email.on("change", function(){
+  let valor = email.val();
 
-console.log(validaNome());
+  var parse_email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 
 
+  if(valor.match(parse_email)){
+    email.removeClass("borda-vermelha");
+    email.addClass("borda-verde");
 
+    return true;
 
-var disponibilidade = $("#disponibilidade");
+  }else{
+    email.removeClass("borda-verde");
+    email.addClass("borda-vermelha");
+    return false;
+  }
 
-disponibilidade.on("click", function(){
-  $("#personalizar").attr("hidden", false);
-  console.log(disponibilidade.val());
 });
+}
+validaEmail();
+/*
+var objeto = new InscricaoForm();
+
+
+var disponibilidade = $(".disponibilidade");
+disponibilidade.on("click", function(){
+  if(disponibilidade.is(":checked") == true) {
+    $("#personalizar").attr("hidden", false);
+  } else {
+    $("#personalizar").attr("hidden", true);
+  }
+});
+
+var outro = $("#outro");
+outro.on("click", function(){
+  if(outro.is(":checked") == true) {
+    $("#Interesses").attr("hidden", false);
+  } else {
+    $("#Interesses").attr("hidden", true);
+  }
+});
+*/
