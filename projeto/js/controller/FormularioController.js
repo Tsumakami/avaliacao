d26e589a0ca9ab33d6b12804a47696dbf85e5ef3
João Paulo Malvesti <jpmf1995@gmail.com>
2019-01-30 00:58:05 -0200

    atividade 4 - tratando a entrada dos interesses

--- projeto/js/controller/FormularioController.js
+++ projeto/js/controller/FormularioController.js
@@ -2,22 +2,20 @@ class FormularioController{
   constructor(){
     this.nome = $("#nome");
     this.email = $("#email");
-    let interesse = $(".interesses");
     this.interesses= new Array();
+
     this.mensagem = $("#mensagem");
 
 
-    for (let i=0;i<interesse.length;i++){
-         if (interesse[i].checked == true){
-            this.interesses.push(interesse[i].value);
-         }
-    }
+
 
   }
     geraInscricao(event){
       let disponibilidades = $(".disponibilidade");
+      let interesse = $(".interesses");
+
       event.preventDefault();
-      //console.log(this.disponibilidade);
+
       for(let i=0;i < disponibilidades.length;i++){
         if(disponibilidades[i].checked){
           if(disponibilidades[i].value == "personalizar"){
@@ -26,6 +24,16 @@ class FormularioController{
             this.disponibilidade = disponibilidades[i].value;
           }
         }
+      }
+
+      for (let i=0;i<interesse.length;i++){
+           if (interesse[i].checked == true){
+             if(interesse[i].value == "outro"){
+              this.interesses.push($("#interesseOutro").val());
+             }else{
+               this.interesses.push(interesse[i].value);
+             }
+          }
       }
 
       let objeto = new InscricaoForm(
